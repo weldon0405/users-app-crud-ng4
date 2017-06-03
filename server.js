4e8a20def1245f4b3896466046e009f08b2f8661
Weldon <weldon0405@gmail.com>
2017-06-02 23:09:39 -0400

    create static folder and  routes

--- server.js
+++ server.js
@@ -15,3 +15,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Static Folder
+app.use(express.static(__dirname + './public/dist'));
+
+
+
+
+
+
+// Routes
+
+app.all('*', (req, res, next) => {
+  res.sendfile(path.resolve('./public/dist/index.html'))
+});
+
+// Server Listening at 1337
+app.listen(1337, () => console.log("Server running at 1337"));
